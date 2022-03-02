84600dfee5fb5eacc272f40b8e7d90ec98d0d467
cweiswadel <100786497+cweiswadel@users.noreply.github.com>
2022-03-02 14:03:40 -0500

    commit for js testing

---
+++ linkGen.js
@@ -0,0 +1,16 @@
+var linksArr = ['index.html','page1.html']; 
+
+function getHtml(template) {
+    return template.join('\n');
+}
+
+function listify(item){
+    return `<li class="page-item"><a class="page-link" href="./${item}">${item}</a></li>`;
+}
+
+function listLinks(inArr){
+    listArr = inArr.map(listify);
+    var htmlOut = [`<ul>`,`<p> List of links </p>`,listArr,`</ul>`]
+
+    document.getElementById('linkList').innerHTML = getHtml(htmlOut);
+}

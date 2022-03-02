c006bb09e68e1745a333b1840d34a180992207cf
cweiswadel <100786497+cweiswadel@users.noreply.github.com>
2022-03-02 15:29:43 -0500

    Fix of js to gen links for webpage

--- linkGen.js
+++ linkGen.js
@@ -8,12 +8,11 @@ function listify(item){
     return `<li class="page-item"><a class="page-link" href="./${item}">${item}</a></li>`;
 }
 
-function listLinks(inArr){
-    var listArr = [];
-    for (let i=0;i<inArr.length;i++){
-        listArr.push(listify(inArr[i]));
-    }
-    var htmlOut = [`<ul>`,`<p> List of links </p>`,listArr,`</ul>`];
-    console.log(htmlOut);
-    document.getElementById('linkList') = getHtml(htmlOut);
-}
+var mapLinksArr = linksArr.map(listify);
+console.log(`mapLinksArr = ${mapLinksArr}`);
+
+// const ulElement = document.createElement("ul");
+const htmlOut = ['<ul>',mapLinksArr,'</ul>'];
+console.log(htmlOut);
+
+document.getElementById('linkList').innerHTML = getHtml(htmlOut);

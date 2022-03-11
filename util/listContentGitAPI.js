f1cf2cb94fc4b8e2b7567ace3a7cfb5fa91e4b7a
Collin Weiwadel <100786497+cweiswadel@users.noreply.github.com>
2022-03-11 12:40:16 -0500

    minor changes to match with remote

--- util/listContentGitAPI.js
+++ util/listContentGitAPI.js
@@ -1,5 +1,7 @@
 //import { request } from "https://cdn.skypack.dev/@octokit/request";
-import request as myModule from "https://cdn.skypack.dev/@octokit/request";
+// import request as myModule from "https://cdn.skypack.dev/@octokit/request";
+// import request from 'https://cdn.skypack.dev/@octokit/request.js';
+
 const ghOwner = "cweiswadel", ghRepo = "cweiswadel.github.io";
 console.log(`ghOwner = ${ghOwner} \n ghRepo = ${ghRepo}`);
 

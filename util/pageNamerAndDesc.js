025bce54e1501d7a3df9f29ac3e5f23d6ef94b61
Collin Weiwadel <100786497+cweiswadel@users.noreply.github.com>
2022-03-21 16:54:29 -0400

    solidified the creation of a dynamic title and body baseline for pages based on a master config json

--- util/pageNamerAndDesc.js
+++ util/pageNamerAndDesc.js
@@ -44,35 +44,48 @@ console.log(pageName);
 document.getElementById('curFilePath').innerHTML = wrapAsParaEle(path);
 document.getElementById('curFileName').innerHTML = wrapAsParaEle(page);
 
-function loadJSON(inFile, callback) {
+// function loadJSON(inFile, callback) {
 
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', inFile, true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
+//     var xobj = new XMLHttpRequest();
+//     xobj.overrideMimeType("application/json");
+//     xobj.open('GET', inFile, true); // Replace 'my_data' with the path to your file
+//     xobj.onreadystatechange = function () {
+//         if (xobj.readyState == 4 && xobj.status == "200") {
+//             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+//             callback(xobj.responseText);
+//         }
+//     };
+//     xobj.send(null);
+// }
 
-async function init(inFile) {
-    loadJSON(inFile, function (response) {
-        // Parse JSON string into object
-        var outData = await JSON.parse(response);
-        console.log(outData)
-    });
-}
+// async function init(inFile) {
+//     loadJSON(inFile, function (response) {
+//         // Parse JSON string into object
+//         var outData = await JSON.parse(response);
+//         console.log(outData)
+//     });
+// }
 
-const pageDirFile = './config/pageInfo.json';
-var jsonData = await init(pageDirFile);
-console.log(jsonData);
+// const pageDirFile = './config/pageInfo.json';
+// var jsonData = await init(pageDirFile);
+// console.log(jsonData);
 
 //testing json data stored locally in this file to test offline/locally
-testJSONData = { "pageDirectory": [{ "pageName": "home", "pageFile": "index.html", "pageSort": 0, "pageInfo": { "pageTitle": "pageTitle", "pageHeader": "pageHeader", "pageDesc": "pageDescription" } }, { "pageName": "1", "pageFile": "page1.html", "pageSort": 1, "pageInfo": { "pageTitle": "pageTitle", "pageHeader": "pageHeader", "pageDesc": "pageDescription" } }, { "pageName": "2", "pageFile": "page2.html", "pageSort": 2, "pageInfo": { "pageTitle": "pageTitle", "pageHeader": "pageHeader", "pageDesc": "pageDescription" } }, { "pageName": "listContentTEST", "pageFile": "listContentTEST.html", "pageSort": 3, "pageInfo": { "pageTitle": "pageTitle", "pageHeader": "pageHeader", "pageDesc": "pageDescription" } }] };
+testJSONData = { "pageDirectory": [ { "pageName": "home", "pageFile": "index.html", "pageSort": 0, "pageInfo": { "pageTitle": "pageTitle", "pageHeader": "pageHeader", "pageDesc": "pageDescription" } }, { "pageName": "1", "pageFile": "page1.html", "pageSort": 1, "pageInfo": { "pageTitle": "Page 1", "pageHeader": "Page 1", "pageDesc": "Welome to the first page." } }, { "pageName": "2", "pageFile": "page2.html", "pageSort": 2, "pageInfo": { "pageTitle": "pageTitle", "pageHeader": "pageHeader", "pageDesc": "pageDescription" } }, { "pageName": "listContentTEST", "pageFile": "listContentTEST.html", "pageSort": 3, "pageInfo": { "pageTitle": "pageTitle", "pageHeader": "pageHeader", "pageDesc": "pageDescription" } } ] };
 
+function createPageTitleHTML(inputData){
+    var dataArr = inputData['pageDirectory'];
+    console.log(dataArr);
+    var winPage = path.split("/").pop();
+    console.log(winPage);
+    const foundPageInfo = dataArr.find(element => element['pageFile'] == winPage);
+    console.log(foundPageInfo);
+    inPageName = foundPageInfo['pageName'];
+    inPageTitle = foundPageInfo['pageInfo']['pageTitle'];
+    titleOut = wrapAsTitleEle(`${inPageName} -- ${inPageTitle}`);
+    console.log(titleOut);
+    return titleOut; //returns string for <title> html ele
+}
 
 
 function createPageBodyBaseHTML(inputData) {
@@ -87,13 +100,8 @@ function createPageBodyBaseHTML(inputData) {
         inPageFile = inPageArr[i]['pageFile'];
         console.log(inPageFile);
         if(inPageFile == winPage){
-            inPageName = inPageArr[i]['pageName'];
-            inPageTitle = inPageArr[i]['pageInfo']['pageTitle'];
-            titleOut = wrapAsTitleEle(`${inPageName} -- ${inPageTitle}`);
-            console.log(titleOut);
-
             inPageHeader = inPageArr[i]['pageInfo']['pageHeader'];
-            inPageDesc = inPageArr[i]['pageInfo']['pageDescription'];
+            inPageDesc = inPageArr[i]['pageInfo']['pageDesc'];
             bodyH1Out = wrapAsH1Ele(inPageHeader);
             arrOut.push(bodyH1Out);
             bodyPOut = wrapAsParaEle(inPageDesc);
@@ -107,4 +115,6 @@ var bodyHTMLArr = createPageBodyBaseHTML(testJSONData);
 var outBodyHtml = outHtml(bodyHTMLArr);
 document.getElementById('bodyHTML').innerHTML = outBodyHtml;
 
+var outTitleHTML = createPageTitleHTML(testJSONData);
+document.getElementById('title').innerHTML = outTitleHTML;
 

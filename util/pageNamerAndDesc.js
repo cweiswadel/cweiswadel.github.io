4992c0b5bdf608fcba540529908b2860c539ae01
Collin Weiwadel <100786497+cweiswadel@users.noreply.github.com>
2022-03-21 13:22:32 -0400

    change to await

--- util/pageNamerAndDesc.js
+++ util/pageNamerAndDesc.js
@@ -37,7 +37,7 @@ function loadJSON(inFile,callback) {
         xobj.overrideMimeType("application/json");
     xobj.open('GET', inFile, true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
-          if (!(xobj.readyState == 4 && xobj.status == "200")) {
+          if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
             callback(xobj.responseText);
           }
@@ -54,5 +54,5 @@ function loadJSON(inFile,callback) {
    }
 
 const pageDirFile = './config/pageInfo.json';
-var jsonData = init(pageDirFile);
+var jsonData = await init(pageDirFile);
 console.log(jsonData);

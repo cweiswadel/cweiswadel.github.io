fb0ace6871b9504b42944db98b33d3092c65dc48
Collin Weiwadel <100786497+cweiswadel@users.noreply.github.com>
2022-03-21 13:20:09 -0400

    change to asynchronous for assignment of read json data to variable

--- util/pageNamerAndDesc.js
+++ util/pageNamerAndDesc.js
@@ -37,7 +37,7 @@ function loadJSON(inFile,callback) {
         xobj.overrideMimeType("application/json");
     xobj.open('GET', inFile, true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
+          if (!(xobj.readyState == 4 && xobj.status == "200")) {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
             callback(xobj.responseText);
           }

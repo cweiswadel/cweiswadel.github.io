66ffe2737a07b3ae001372d407339228c6f4c6cb
Collin Weiwadel <100786497+cweiswadel@users.noreply.github.com>
2022-03-21 12:51:22 -0400

    committing to get into main to test if loadJSON function works on github hosting as opposed to locally

--- util/pageNamerAndDesc.js
+++ util/pageNamerAndDesc.js
@@ -28,8 +28,30 @@ console.log( path );
 console.log( page );
 console.log( pageName );
 
-
-
-
 document.getElementById('curFilePath').innerHTML = wrapAsParaEle(path);
 document.getElementById('curFileName').innerHTML = wrapAsParaEle(page);
+
+function loadJSON(inFile,callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', inFile, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+ 
+ function init(inFile) {
+    loadJSON(inFile,function(response) {
+     // Parse JSON string into object
+       var actual_JSON = JSON.parse(response);
+    });
+   }
+
+const pageDirFile = './config/pageInfo.json';
+var jsonData = init(pageDirFile);
+console.log(jsonData);

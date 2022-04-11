902f19a44fe541cb2cbed05db16e80d73802d2a5
Collin Weiwadel <100786497+cweiswadel@users.noreply.github.com>
2022-04-11 16:31:22 -0400

    dynamic check box of menu type complete

--- util/menuLister.js
+++ util/menuLister.js
@@ -16,17 +16,11 @@ function displayMenuRaw(menuOutHTML) {
     anchorElement.appendChild(div);
 }
 
-//function to select an item from a drop down and update a text box/form from the selection
-document.getElementById('myList').onchange = function() {
-    var mylist = document.getElementById("myList");
-    document.getElementById("favourite").value = mylist.options[mylist.selectedIndex].text;
-}
-
-function listMenuSection(menuDataObj){
+function listMenuSection(menuDataObj) {
     console.log(`listing menu section (desserts vs drinks vs etc.)`)
     //take a menuData (object) variable and then allows selection of an element from the "menuSections" value 
     var menuSectionArr = menuDataObj["menuSections"];
-    
+
     var anchorEle = document.getElementById('menuSections');
     var opt = document.createElement('option');
     var optInfoText = `--Please select menu--`;
@@ -37,10 +31,10 @@ function listMenuSection(menuDataObj){
     anchorEle.appendChild(opt);
 
     var menuSectionName;
-    for(let i=0;i<menuSectionArr.length;i++){
+    for (let i = 0; i < menuSectionArr.length; i++) {
         var loopOpt = document.createElement('option');
         console.log(menuSectionArr[i]);
-        menuSectionName = menuSectionArr[i]['menuSectionName']; 
+        menuSectionName = menuSectionArr[i]['menuSectionName'];
         console.log(menuSectionName);
         loopOpt.id = menuSectionName;
         loopOpt.value = menuSectionName;
@@ -48,12 +42,27 @@ function listMenuSection(menuDataObj){
         console.log(loopOpt);
         anchorEle.appendChild(loopOpt);
     }
-
 }
 
+document.getElementById('menuSections').onchange = function () {
+    var menuList = document.getElementById("menuSections"); //for taking data from select field "menuSections"
+    var parentNode = document.getElementById("selectedMenu"); // establishing parent node/anchor element
+    var childNode = document.createElement('input'); //creating a childNode to dynamically change based on select "menuSelections"
 
+    //establishing values and parameters for the child node -- this will be the reflected output 
+    childNode.type = "text", childNode.size = "20", childNode.id = "selectedMenuChild", childNode.value = menuList.options[menuList.selectedIndex].text;
 
+    var textNode = document.createElement('p') //creating a descriptive text element
+    textNode.innerHTML = "Selected Menu: "; //text of p element
 
+    //since this function is ONCHANGE, remove any listed choices and then re-append/display the newest choice
+    //this while loop clears ALL child nodes from parentNode
+    while (parentNode.firstChild) {
+        parentNode.removeChild(parentNode.firstChild);
+    }
+    parentNode.appendChild(textNode);
+    parentNode.appendChild(childNode);
+}
 
 const menuFile = "./config/menuInfo.json"
 var menuData = requestData(menuFile);
@@ -62,5 +71,5 @@ console.log(menuData);
 // const menuDataStr = JSON.stringify(menuData, null, 2); //spacing level = 2, which is pretty print
 // console.log(menuDataStr);
 
-displayMenuRaw(menuData);
+// displayMenuRaw(menuData);
 listMenuSection(menuData);

3b70fab70eb6634e02d2d6c225046eefa604558f
Collin Weiwadel <100786497+cweiswadel@users.noreply.github.com>
2022-04-29 16:20:00 -0400

    way to much shit and brain hurting

--- util/menuLister.js
+++ util/menuLister.js
@@ -16,60 +16,177 @@ function displayMenuRaw(menuOutHTML) {
     anchorElement.appendChild(div);
 }
 
-function listMenuSection(menuDataObj) {
+function capitalize(string){
+    return string.charAt(0).toUpperCase()+string.slice(1);
+}
+
+//function to build list of options in the dropdown(select with child option elements)
+function listMenuSection(menuDataObj, anchorPointID) {
     console.log(`listing menu section (desserts vs drinks vs etc.)`)
     //take a menuData (object) variable and then allows selection of an element from the "menuSections" value 
     var menuSectionArr = menuDataObj["menuSections"];
 
-    var anchorEle = document.getElementById('menuSections');
-    var opt = document.createElement('option');
-    var optInfoText = `--Please select menu--`;
-    opt.id = "helpText"
-    opt.value = null;
-    opt.innerHTML = optInfoText;
-    console.log(opt)
-    anchorEle.appendChild(opt);
+    //anchorPointID is the ID of the element in the HTML that the 
+    var anchorEle = document.getElementById(anchorPointID);
+    var opt = document.createElement('option'); //create an option element (dropdown)
+    var optInfoText = `--Please select menu--`; //default text to be displayed in the dropdown
+    opt.id = "helpText", opt.value = null, opt.innerHTML = optInfoText; //create and set values for dropdown element 
+    // console.log(opt)
+    anchorEle.appendChild(opt); //appending the dropdown into the anchor point
 
-    var menuSectionName;
+    var menuSectionName; //init a var for the name of items to be added in the dropdown 
     for (let i = 0; i < menuSectionArr.length; i++) {
-        var loopOpt = document.createElement('option');
-        console.log(menuSectionArr[i]);
-        menuSectionName = menuSectionArr[i]['menuSectionName'];
-        console.log(menuSectionName);
-        loopOpt.id = menuSectionName;
-        loopOpt.value = menuSectionName;
-        loopOpt.innerHTML = menuSectionName;
-        console.log(loopOpt);
-        anchorEle.appendChild(loopOpt);
+        var loopOpt = document.createElement('option'); //while in the loop create an option element
+        // console.log(menuSectionArr[i]);
+        menuSectionName = menuSectionArr[i]['menuSectionName']; //get the menuSection name from the menuSectionArr
+        // console.log(menuSectionName);
+        loopOpt.id = menuSectionName, loopOpt.value = menuSectionName, loopOpt.innerHTML = menuSectionName; //set the values of the option to be added to the dropdown list
+        // console.log(loopOpt);
+        anchorEle.appendChild(loopOpt); //adding element into dropdown of possibilities
     }
 }
 
-document.getElementById('menuSections').onchange = function () {
-    var menuList = document.getElementById("menuSections"); //for taking data from select field "menuSections"
-    var parentNode = document.getElementById("selectedMenu"); // establishing parent node/anchor element
-    var childNode = document.createElement('input'); //creating a childNode to dynamically change based on select "menuSelections"
-
-    //establishing values and parameters for the child node -- this will be the reflected output 
-    childNode.type = "text", childNode.size = "20", childNode.id = "selectedMenuChild", childNode.value = menuList.options[menuList.selectedIndex].text;
-
-    var textNode = document.createElement('p') //creating a descriptive text element
-    textNode.innerHTML = "Selected Menu: "; //text of p element
-
-    //since this function is ONCHANGE, remove any listed choices and then re-append/display the newest choice
-    //this while loop clears ALL child nodes from parentNode
-    while (parentNode.firstChild) {
-        parentNode.removeChild(parentNode.firstChild);
-    }
-    parentNode.appendChild(textNode);
-    parentNode.appendChild(childNode);
+function getNextNodeID(inputNodeID) {
+    let curEle = document.getElementById(inputNodeID);
+    let siblingEleID = curEle.nextElementSibling.id;
+    return siblingEleID;
 }
+
+// function formatMenuSelectionDataObj_forTable(menuSectionObj) {
+//     if(menuSectionObj==undefined){
+//         //menuSectionObj has been initialized yet
+//         console.log('Awaiting input of menuSectionSelector');
+//     } else {
+//         console.log(menuSectionObj);
+//         var tableHeader = menuSectionObj.menuSectionName;
+//         console.log(tableHeader);
+//     }
+
+// }
+
+function onChangeOfDropDown(selectEleID, displayNodeID, displayText, dataObj) {
+    document.getElementById(selectEleID).onchange = function () {
+        var userSelectList = document.getElementById(selectEleID); //for taking data from select field "menuSections"
+        // var parentNode = document.getElementById("selectedMenu"); // establishing parent node/anchor element, which will be the next node after selectEleID (nextElementSibling)
+        var parentNode = document.getElementById(getNextNodeID(selectEleID)); // establishing parent node/anchor element, which will be the next node after selectEleID (nextElementSibling)
+        var childNode = document.createElement('input'); //creating a childNode to dynamically change based on select "menuSelections"
+
+        //establishing values and parameters for the child node -- this will be the reflected output 
+        childNode.type = "text", childNode.size = "20", childNode.id = displayNodeID, childNode.value = userSelectList.options[userSelectList.selectedIndex].text, childNode.readOnly = true;
+
+        var textNode = document.createElement('p') //creating a descriptive text element
+        textNode.innerHTML = displayText; //text of p element
+
+        var dataNode = document.createElement('article');
+
+        var dataObjArr = dataObj['menuSections'], menuSectionName, menuSectionElement, outData;
+        console.log(dataObjArr);
+        for (let i = 0; i < dataObjArr.length; i++) {
+            menuSectionElement = dataObjArr[i];
+            menuSectionName = dataObjArr[i]['menuSectionName'];
+            outData = 'No menu selected...yet';
+            if (menuSectionName === childNode.value) {
+                console.log(menuSectionName);
+                outData = menuSectionElement;
+                console.log(outData);
+                break;
+            }
+        }
+        // console.log(JSON.stringify(outData, null, 2));
+        dataNode.innerHTML = outData;
+        dataNode.id = 'rawDataNode';
+
+        //since this function is ONCHANGE, remove any listed choices and then re-append/display the newest choice
+        //this while loop clears ALL child nodes from parentNode
+        while (parentNode.firstChild) {
+            parentNode.removeChild(parentNode.firstChild);
+        }
+        //add nodes to get correct output/display
+        parentNode.appendChild(textNode);
+        parentNode.appendChild(childNode);
+        parentNode.appendChild(dataNode);
+
+        //testing
+        var tableNode = document.createElement('table')
+        tableNode.id = 'menuSecTable';
+        console.log(tableNode);
+        // console.log(outData);
+        var tableHeader = outData.menuSectionName;
+        var fullDataArr = outData.menuItems;
+        var foodTypesArr = [], foodTypeCtObj = {};
+
+
+        for (let i = 0; i < fullDataArr.length; i++) {
+            var foodTypeName = fullDataArr[i].menuItemType.foodType;
+            foodTypeCtObj[foodTypeName] = foodTypeCtObj[foodTypeName] ? foodTypeCtObj[foodTypeName] + 1 : 1;
+        }
+
+        console.log(foodTypeCtObj);
+
+        console.log(tableHeader);
+        console.log(fullDataArr);
+        console.log(foodTypesArr);
+
+        var totalColSpan = fullDataArr.length;
+
+        tableNode.setAttribute("class", "table table-hover table-dark");
+        var tableNode_tHead = tableNode.createTHead();
+        var headerMenuSec_tr =  document.createElement('tr');
+        var headerMenusSec_th = document.createElement('th');
+        headerMenusSec_th.innerHTML = capitalize(tableHeader),headerMenusSec_th.colSpan = totalColSpan, headerMenusSec_th.scope='col', headerMenusSec_th.style.textAlign='center';
+        tableNode_tHead.appendChild(headerMenuSec_tr);
+        headerMenuSec_tr.appendChild(headerMenusSec_th);
+
+        // var headerFoodTypeRow;
+        var headerFoodType_tr = document.createElement('tr');
+        for (const [foodTypeName, count] of Object.entries(foodTypeCtObj)) {
+            console.log(`${foodTypeName} : ${count}`);
+            var headerFoodType_th = document.createElement('th');
+            headerFoodType_th.colSpan = count, headerFoodType_th.innerText = capitalize(foodTypeName);
+            console.log(headerFoodType_th.innerHtml);
+            headerFoodType_tr.appendChild(headerFoodType_th);
+        }
+        tableNode_tHead.appendChild(headerFoodType_tr);
+
+        // headerMenuCell.innerHTML = tableHeader;
+
+        // var headerFoodTypeRow = headerMenuSec.insertRow(1);
+
+        // for(let i=0; i<totalColSpan; i++){
+
+
+        // }
+
+        parentNode.appendChild(tableNode);
+
+
+
+
+        return outData; //return the dataNode as an html element to manipulate separately 
+    }
+}
+
+//function to take the dataNode input and manipulate it into human readable display (the actual menu display)
+//take the input of a dataNode (element agnostic)
+
+
+
 
 const menuFile = "./config/menuInfo.json"
-var menuData = requestData(menuFile);
-console.log(menuData);
-// const menuDataStr = JSON.stringify(menuData);
-// const menuDataStr = JSON.stringify(menuData, null, 2); //spacing level = 2, which is pretty print
-// console.log(menuDataStr);
+var menuDataObj = requestData(menuFile);
+console.log(menuDataObj);
 
 // displayMenuRaw(menuData);
-listMenuSection(menuData);
+const menuSecID = 'menuSections', selectMenuSecID = 'selectedMenuSecChild', selectMenuSecTxt = "Selected Menu Section: ";
+var runMenuListFn = listMenuSection(menuDataObj, menuSecID);
+runMenuListFn;
+
+var runOnChangeMenuSecList = onChangeOfDropDown(menuSecID, selectMenuSecID, selectMenuSecTxt, menuDataObj);
+runOnChangeMenuSecList;
+
+
+
+
+
+
+// var runOnChangeMenuInput = onChangeOfInput()
